861546f6f2f9fbc5f30a64dc3e3e245602320eee
细雨蒙蒙飘 <huangcheng0908@163.com>
2016-03-18 12:15:11 +0800

    Initial Commit

---
+++ YYCommentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYCommentControl
+//
+//  Created by mac on 16/3/18.
+//  Copyright © 2016年 黄城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYCommentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYCommentControl
+//
+//  Created by mac on 16/3/18.
+//  Copyright © 2016年 黄城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
